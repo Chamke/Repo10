180d7d4bb542dce2375eba127c69d81ca2e286bf
Chamke <terzimehic.amil@hotmail.com>
2022-04-06 22:05:04 +0200

    change

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,3 +1,9 @@
+var  myObj = {
+    title: "Titanic", 
+    song: "My Heart Will Go On", 
+    genre: "drama"
+}
+
 function myFunction(myObj, checkProp){
 
      if (myObj.hasOwnProperty(checkProp)){
@@ -7,8 +13,8 @@ function myFunction(myObj, checkProp){
     }
 }
 
-console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "title"));
-console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "song"));
-console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "genre"));
-console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "actor"));
+console.log(myFunction(myObj, "title"));
+console.log(myFunction(myObj, "song"));
+console.log(myFunction(myObj, "genre"));
+console.log(myFunction(myObj, "actor"));
 module.exports = myFunction;
